6957ee86a3cb232de31fbd1648fa7dfd50c1ce15
AnupKarki <i.anup315@gmail.com>
2015-11-05 12:08:50 +0545

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-#!bin/bash
+#!/usr/bash
+
 chmod a+x ./src/average_degree.py
 chmod a+x ./src/tweets_cleaned.py
 
-# open average degree caalucations
-python ./src/average_degree.py 
-	
-# open tweet cleaner
-python ./src/tweets_cleaned.py 
-	
+# open tweets cleaning program    Input file                outputFile
+python ./src/tweets_cleaned.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+
+# open average degree count program
+python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
+
     
